97a43f39dabad8c0ad31ffa939493b46d0c3398b
WeiTradescant <weitradescant@163.com>
2018-01-04 13:27:24 +0800

    function makeGrid

--- designs.js
+++ designs.js
@@ -4,7 +4,25 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+	let col = $('#input_width').val();
+	let row = $('#input_height').val();
+	$('#pixel_canvas td').remove();
+	$('#pixel_canvas tr').remove();
+	for(let i = 1; i <= row; i++) {
+		$('#pixel_canvas').append('<tr></tr>');
+	}
+	for(let i = 1; i <= col; i++) {
+		$('#pixel_canvas tr').append('<td></td>');
+	}
 }
+
+function makeColor() {
+	/**/
+}
+
+function reset() {
+	/**/
+}
+
+$('#input_width,#input_height').on('change',makeGrid);
+$('#input_reset').click(reset);
